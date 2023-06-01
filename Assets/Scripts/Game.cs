e822f5827f457b94c346c1d5af313a085c3b9bf4
lordsynix <leSynix22@gmail.com>
2023-06-01 22:03:04 +0200

    Library UI

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Game
+{
+    public static string Name;
+
+    public static int BundleVersion;
+    public static bool isDownloaded;
+    public static bool isUpdated;
+
+}
---
+++ Assets/Scripts/UI/GameItem.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class GameItem
+{
+    public string gameName;
+
+    public Sprite gameLogo;
+
+}
