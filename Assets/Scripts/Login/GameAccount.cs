ad9f8db6dec56835239c31979a8c1e6c2d5aaddc
lordsynix <leSynix22@gmail.com>
2023-06-06 22:15:45 +0200

    Login System (Localhost)

---
+++ Assets/Scripts/Login/GameAccount.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class GameAccount
+{
+    public string _id;
+    public int adminFlag;
+    public string username;
+}
---
+++ Assets/Scripts/Login/Login.cs
@@ -0,0 +1,178 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class Login : MonoBehaviour
+{
+    [SerializeField] private string loginEndpoint = "http://127.0.0.1:13756/account/login";
+    [SerializeField] private string createEndpoint = "http://127.0.0.1:13756/account/create";
+
+    [SerializeField] private InputField usernameInputField;
+    [SerializeField] private InputField passwordInputField;
+    [SerializeField] private Text alertText;
+    [SerializeField] private Button loginButton;
+    [SerializeField] private Button createButton;
+
+    public void OnLoginClick()
+    {
+        alertText.text = "Signing in...";
+        ActivateButtons(false);
+
+        StartCoroutine(TryLogin());
+    }
+
+    public void OnCreateClick()
+    {
+        alertText.text = "Creating account...";
+        ActivateButtons(false);
+
+        StartCoroutine(TryCreate());
+    }
+
+    private IEnumerator TryLogin()
+    {
+        string username = usernameInputField.text;
+        string password = passwordInputField.text;
+
+        if (username.Length < 3 || username.Length > 24)
+        {
+            alertText.text = "Invalid username";
+            ActivateButtons(true);
+            yield break;
+        }
+
+        if (password.Length < 3 || password.Length > 24)
+        {
+            alertText.text = "Invalid password";
+            ActivateButtons(true);
+            yield break;
+        }
+
+        WWWForm form = new WWWForm();
+        form.AddField("rUsername", username);
+        form.AddField("rPassword", password);
+
+        UnityWebRequest request = UnityWebRequest.Post(loginEndpoint, form);
+        var handler = request.SendWebRequest();
+
+        float startTime = 0.0f;
+        while (!handler.isDone)
+        {
+            startTime += Time.deltaTime;
+
+            if (startTime > 10.0f) break;
+
+            yield return null;
+        }
+
+        if (request.result == UnityWebRequest.Result.Success)
+        {
+            LoginResponse response = JsonUtility.FromJson<LoginResponse>(request.downloadHandler.text);
+
+            if (response.code == 0)
+            {
+                ActivateButtons(false);
+                alertText.text = "Welcome " + ((response.data.adminFlag == 1) ? "Admin" : "");
+            }
+            else
+            {
+                switch (response.code)
+                {
+                    case 1:
+                        alertText.text = "Invalid credentials";
+                        ActivateButtons(true);
+                        break;
+                    default:
+                        alertText.text = "Corruption detected";
+                        ActivateButtons(false);
+                        break;
+                }
+            }
+        }
+        else
+        {
+            alertText.text = "Error connecting to the server...";
+            ActivateButtons(true);
+        }
+
+        yield return null;
+    }
+
+    private IEnumerator TryCreate()
+    {
+        string username = usernameInputField.text;
+        string password = passwordInputField.text;
+
+        if (username.Length < 3 || username.Length > 24)
+        {
+            alertText.text = "Invalid username";
+            ActivateButtons(true);
+            yield break;
+        }
+
+        if (password.Length < 3 || password.Length > 24)
+        {
+            alertText.text = "Invalid password";
+            ActivateButtons(true);
+            yield break;
+        }
+
+        WWWForm form = new WWWForm();
+        form.AddField("rUsername", username);
+        form.AddField("rPassword", password);
+
+        UnityWebRequest request = UnityWebRequest.Post(createEndpoint, form);
+        var handler = request.SendWebRequest();
+
+        float startTime = 0.0f;
+        while (!handler.isDone)
+        {
+            startTime += Time.deltaTime;
+
+            if (startTime > 10.0f) break;
+
+            yield return null;
+        }
+
+        if (request.result == UnityWebRequest.Result.Success)
+        {
+            Debug.Log(request.downloadHandler.text);
+            CreateResponse response = JsonUtility.FromJson<CreateResponse>(request.downloadHandler.text);
+
+            if (response.code == 0)
+            {
+                alertText.text = "Account has been created";
+            }
+            else
+            {
+                switch (response.code)
+                {
+                    case 1:
+                        alertText.text = "Invalid credentials";
+                        break;
+                    case 2:
+                        alertText.text = "Username is already taken";
+                        break;
+                    default:
+                        alertText.text = "Corruption detected";
+                        break;
+                }
+            }
+        }
+        else
+        {
+            alertText.text = "Error connecting to the server...";
+        }
+
+        ActivateButtons(true);
+
+        yield return null;
+    }
+
+    private void ActivateButtons(bool toggle)
+    {
+        loginButton.interactable = toggle;
+        createButton.interactable = toggle;
+    }
+}
